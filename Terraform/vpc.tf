0033ca6f481bcb8bd310535012e18487b209defa
dev2 <dev2@mail.com>
2023-08-17 20:52:10 +0530

    vpcfile updated

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -1,12 +1,10 @@
 provider "aws" {
     region = "ap-south-1"
-      
-}
+    }
 resource "aws_vpc" "my_vpc" {
   cidr_block       = "10.0.0.0/16"
-  enable_dns_support = "true"
-  enable_dns_hostnames = "true"
+  instance_tenancy = "default"
   tags = {
-    Name = my_vpc_new
+    Name = "my_vpc_new"
   }
 }
