37778ba41de5f135e95df2d7eb8d1e32a6d9ab37
dev2 <dev2@mail.com>
2023-08-18 20:23:21 +0530

    createinstancefile created2

--- Terraform/createinstance.tf
+++ Terraform/createinstance.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "my_instance" {
     ami = "ami-08e5424edfe926b43"
     instance_type = "t2.micro"
+    count = 2
     tags = {
-        Name = "my_instance"
+        Name = "my_instance.${-index}"
     }
 }
